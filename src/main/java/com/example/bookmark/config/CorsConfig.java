7354dbb7d16129a6ff9ee3f9a7c6efa84b889393
diptendu01 <ddas2367@gmail.com>
2026-02-20 18:51:51 +0530

    Update CORS configuration to allow additional origin

--- src/main/java/com/example/bookmark/config/CorsConfig.java
+++ src/main/java/com/example/bookmark/config/CorsConfig.java
@@ -10,8 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://smart-bookmark-delta-nine.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
